bd85fa2d6a7ce14612c0b0d0687f62cfbbd128f8
Gilberto Theves <betoth@gmail.com>
2018-01-16 22:34:55 -0200

    Correct commit new collum off_value

--- db/migrate/20180116224914_add_new_fields_to_offer.rb
+++ db/migrate/20180116224914_add_new_fields_to_offer.rb
@@ -1,6 +1,5 @@
 class AddNewFieldsToOffer < ActiveRecord::Migration[5.1]
   def change
     add_column :offers, :off_value, :float
-    add_column :offers, :off_id_user_delete, :integer
-  end
+    end
 end
